c62329411e0709505a9c82a87293f29eb378f833
Calhoun <44627017+ChanghaoShih@users.noreply.github.com>
2019-03-19 16:08:15 -0700

    Add files via upload

--- decoder/Train.py
+++ decoder/Train.py
@@ -53,46 +53,49 @@ def main():
     dictionary =list(dictionary.keys())[0:20000-2]
     dictionary = ['EOS'] + dictionary + ['UNK']
     dict_size = len(dictionary)
+    if 'dog' in dictionary:
+        print('dog in dictionary')
     print('Dictionary successfully loaded.\n'+'*'*50)
 
     # build dataset and dataloader
     print('Building dataloader...')
     bs = config['batch_size']
+    n_worker = config['num_worker']
     dataset = TextSet(file_dir=config['dir'], dictionary=dictionary)
     print('Total: {}'.format(len(dataset)))
-    dataloader = utils.data.DataLoader(dataset, batch_size=bs, shuffle=True, collate_fn=story_collate, num_workers=16, pin_memory=True)
+    dataloader = utils.data.DataLoader(dataset, batch_size=bs, shuffle=True, collate_fn=story_collate, num_workers=n_worker, pin_memory=True)
     print('Dataloader successfully built.\n'+'*'*50)
     
-    # read vocab
-    print('Reading vocabulary in the corpus...')
-    if not config['vocab']:
-        with open(config['dir'], encoding="utf-8") as g:
-            book = g.read()
-            vocab = book.split()
-            if 'EOS' not in vocab:
-                vocab.append('EOS')
-        vocab = list(set(vocab))
-        with open('vocab.pkl', 'wb') as f:
-            pickle.dump(vocab, f)
-    else:
-        with open('vocab.pkl', 'rb') as f:
-            vocab = pickle.load(f)
-    print('Vocabulary successfully read.\n'+'*'*50)
+#     # read vocab
+#     print('Reading vocabulary in the corpus...')
+#     if not config['vocab']:
+#         with open(config['dir'], encoding="utf-8") as g:
+#             book = g.read()
+#             vocab = book.split()
+#             if 'EOS' not in vocab:
+#                 vocab.append('EOS')
+#         vocab = list(set(vocab))
+#         with open('vocab.pkl', 'wb') as f:
+#             pickle.dump(vocab, f)
+#     else:
+#         with open('vocab.pkl', 'rb') as f:
+#             vocab = pickle.load(f)
+#     print('Vocabulary successfully read.\n'+'*'*50)
 
     # load skipvector
     print('Loading skip-thoughts...')
     with torch.no_grad():
-        uniskip = UniSkip(skip_dir, vocab).to(device)
+        uniskip = UniSkip(skip_dir, dictionary).to(device)
     print('Skip-thoughts successfully loaded.\n'+'*'*50)
     
     # initialize
     print('Initializing...')
-    onehot = Onehot(dictionary=dictionary)
-
+    
     model = Model(dict_size, 620, 2400, bs).to(device)
     
-    weight = torch.tensor([5]+[1/(1+np.exp((x-dict_size)/(dict_size/5))) for x in range(dict_size-1)], device=device)
+    weight = torch.tensor([1/(1+np.exp((dict_size-x)/dict_size)) for x in range(dict_size)], device=device)
     criterion = nn.NLLLoss(weight=weight, ignore_index=dict_size-1)
+#     criterion = nn.NLLLoss(ignore_index=dict_size-1)
 
     optimizer = optim.Adam(model.parameters(), lr=config['lr'])
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=config['patience'])
@@ -106,9 +109,14 @@ def main():
         for i, story in enumerate(dataloader):
 
             # formatting
-            story, story_len = pad(story, dict_size-1)
+            if bs == 1:
+                story = torch.tensor(story)
+                story_len = [len(story)]
+            else:
+                story, story_len = pad(story, dict_size-1)
+            
             story = story.to(device)
-            encoding = uniskip(story.view(bs, -1), lengths=story_len).to(device) # input size: (batch_size, 2400)
+            encoding = uniskip(story.view(bs, -1), lengths=story_len).detach().to(device) # input size: (batch_size, 2400)
             input = torch.cat((torch.zeros((bs, 1), device=device).long(), story[:, :-1]), dim=1)
             input = input.transpose(0, 1).unsqueeze(2).to(device)
 
@@ -125,12 +133,12 @@ def main():
             
             scheduler.step(loss)
             
-            if not i % 100:
+            if not i % 10:
                 print(Loss[-1])
             
             
             # save model
-            if not i % 10000:
+            if not i % 2000:
                 model_name = "epoch_{}-batch_{}-{}.pt".format(epoch, i, time.strftime("%Y%m%d-%H%M%S"))
                 torch.save(model.state_dict(), os.path.join(model_path, model_name))
 #                 print(output)
